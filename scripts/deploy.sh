dd48bc96fc7cb4222c6df77ed42c653ecd0a0b82
PhilipVis <philipvissenaekens@msn.com>
2018-08-25 18:42:45 +0200

    verummeum

---
+++ scripts/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+cd ..
+
+# Build the project.
+hugo 
+
+# Add changes to the source git.
+git add .
+
+# Commit changes.
+msg="verummeum"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push to source repo.
+git push origin master
+
+# Go To Public folder
+cd public
+
+# Add changes to the website git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push to website repo.
+git push origin master
